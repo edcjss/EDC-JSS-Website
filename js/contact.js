d15a843ee92a12fb5c9e0200867c7facf13909eb
PragatiVerma18 <itispragativerma@gmail.com>
2021-05-19 17:53:39 +0530

    formatted code

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,16 @@
 function openNav() {
-
-  if(document.getElementById('mobile__menu').className=="overlay"){ 
-       document.getElementById("mobile__menu").classList.add('overlay_clicked');
-       document.getElementById('menu').innerText='close';
-       document.getElementById('menu').style.backgroundColor='#17a2b8';
-       document.getElementById('menu').style.opacity='.7';
-       document.getElementById('menu').style.color='white';
-       document.getElementById('menu').style.borderColor='#17a2b8';
-
-
+  if (document.getElementById("mobile__menu").className == "overlay") {
+    document.getElementById("mobile__menu").classList.add("overlay_clicked");
+    document.getElementById("menu").innerText = "close";
+    document.getElementById("menu").style.backgroundColor = "#17a2b8";
+    document.getElementById("menu").style.opacity = ".7";
+    document.getElementById("menu").style.color = "white";
+    document.getElementById("menu").style.borderColor = "#17a2b8";
+  } else {
+    document.getElementById("mobile__menu").classList.remove("overlay_clicked");
+    document.getElementById("mobile__menu").classList.add("overlay");
+    document.getElementById("menu").innerText = "menu";
+    document.getElementById("menu").style.backgroundColor = "white";
+    document.getElementById("menu").style.color = "black";
   }
-
-  else{
-       document.getElementById("mobile__menu").classList.remove('overlay_clicked');  
-       document.getElementById("mobile__menu").classList.add('overlay');
-       document.getElementById('menu').innerText='menu';
-       document.getElementById('menu').style.backgroundColor='white';
-       document.getElementById('menu').style.color='black';
-
-  }
-
 }
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,73 +1,71 @@
-const typeWriter = function(txtElement, words, wait = 2000){
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-}
+const typeWriter = function (txtElement, words, wait = 2000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
 
 //Type Method
 
-typeWriter.prototype.type = function(){
-    //console.log('Hello World');
-    
-    // Current Word Index
-    // Get the words index(there are 3 separate entities here as written in data-words)
-    const current = this.wordIndex % this.words.length;
-    //console.log(current);
-    // Get full text of current word
-    const fullTxt = this.words[current];
+typeWriter.prototype.type = function () {
+  //console.log('Hello World');
 
-    //console.log(fullTxt);
+  // Current Word Index
+  // Get the words index(there are 3 separate entities here as written in data-words)
+  const current = this.wordIndex % this.words.length;
+  //console.log(current);
+  // Get full text of current word
+  const fullTxt = this.words[current];
 
-    //Check if word is deleting
-    if(this.isDeleting){
-        //remove characters
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    }
-    else{
-       // add characters
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  //console.log(fullTxt);
 
-    // Insert text in element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  //Check if word is deleting
+  if (this.isDeleting) {
+    //remove characters
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // add characters
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-    // Custom Type Speed for words
-    let typeSpeed = 300;
-    if(this.isDeleting){
-        typeSpeed /= 2;
-    }
+  // Insert text in element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    // If word is complete
-    if(!this.isDeleting && this.txt === fullTxt){
-        // Make pause at end of word
-        typeSpeed = this.wait;
-        // Set isDeleting to true to delete txt
-        this.isDeleting = true;
-    }
-    else if(this.isDeleting && this.txt === ""){
-        // Make isDeleting false
-        this.isDeleting = false;
-        // move to next word
-        this.wordIndex++;
-        // Pause to start next word
-        typeSpeed = 500;
-    }
+  // Custom Type Speed for words
+  let typeSpeed = 300;
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
 
-    setTimeout(() => this.type(), typeSpeed);
-}
+  // If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // Make pause at end of word
+    typeSpeed = this.wait;
+    // Set isDeleting to true to delete txt
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    // Make isDeleting false
+    this.isDeleting = false;
+    // move to next word
+    this.wordIndex++;
+    // Pause to start next word
+    typeSpeed = 500;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
 
 //Initialize on DOM Load
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 //Initialize App
-function init(){
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-    // Initialize Typewriter
-    new typeWriter(txtElement, words, wait);
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Initialize Typewriter
+  new typeWriter(txtElement, words, wait);
 }
